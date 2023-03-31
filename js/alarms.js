6bf7601db7544598980b2c1c9408b050b21a2688
kylevgross <54626180+kylevgross@users.noreply.github.com>
2023-03-31 07:32:43 -0400

    Add files via upload

---
+++ js/alarms.js
@@ -0,0 +1,29 @@
+//Begin auto generated javascript
+require(["baja!"], function (baja) {"use strict";
+      var alarmSub = new baja.Subscriber();
+      function update(alarms) {
+            var alarmString = alarms.getOut().getValue().toString();
+            if (alarmString.includes('.')){
+                var alarmDisplay = alarmString.substring(0, alarmString.indexOf('.'));
+            }
+            else
+                alarmDisplay = alarmString;
+            document.getElementById("AllAlarms").innerHTML = "Alarms (" + alarmDisplay + ")";
+            var val = alarms.getOut().getValue();
+              if (val > 0.5){
+                document.getElementById("AllAlarms").style="animation:alarm 2.5s infinite;"
+              }
+              else {
+                document.getElementById("AllAlarms").style="animation:none;"
+              }
+            }
+            alarmSub.attach('changed', function (prop) {
+            if (prop.getName() === 'out') { update(this); }
+             });
+            baja.Ord.make("station:|slot:/Services/AlarmService/AlarmCount").get({
+               subscriber: alarmSub
+            }).then(update);
+      });
+
+
+
